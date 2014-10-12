857ae48dc42e51abc4a0688d6f079a77af9bbedd
ErikPeterson <eriksalgstrom@gmail.com>
2014-10-12 02:05:26 -0400

    Adding browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var browserify = require('gulp-browserify');
+var rename = require('gulp-rename');
 
 gulp.task('jshint', function(){
     gulp.src("./app/**/*.js").pipe(jshint()).pipe(jshint.reporter("default"));
 });
 
 gulp.task('compile-dev', function(){
-    gulp.src('app/src/main.js').pipe(broswerify({
-        debug: true
-    }));
+    gulp.src('app/src/main.js').pipe(browserify({
+        debug: true,
+    })).pipe(rename('app.js'))
+    .pipe(gulp.dest('./app/dist'));
 });
 
 gulp.task('watch', function(){
-    gulp.watch('app/**/*.js', ['jshint']);
+    gulp.watch('app/**/*.js', ['jshint', 'compile-dev']);
 });
